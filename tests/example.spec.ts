7d8251c73dcfdb9b5852c5b52ce7ad6be5243571
DharshiniJeyachandran <dharshini.jc@gmail.com>
2025-08-18 15:38:15 +0200

    Adding example.spec.ts

---
+++ tests/example.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test('homepaget title', async ({ page }) => {
+  await page.goto("http://40.76.118.177:9090/");
+  await page.getByText("🌴Far Away💼").isVisible();
+  await page.waitForTimeout(5000);
+});
+
+test('input field', async({page}) => {
+    await page.goto("http://40.76.118.177:9090/");
+  await page.getByText("🌴Far Away💼").isVisible();
+  await page.getByPlaceholder("Items...").fill("Backpack");
+})
+
+test.only('add items', async({page}) => {
+   await page.goto("http://40.76.118.177:9090/");
+  await page.getByText("🌴Far Away💼").isVisible();
+  await page.getByPlaceholder("Items...").fill("Backpack");
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.locator('input[type="checkbox"][value="false"]').click();
+await page.waitForTimeout(5000);
+})
+
+test('Add items', async ({page}) => {
+  await page.goto("http://40.76.118.177:9090/");
+  await page.getByText("🌴Far Away💼").isVisible();
+  await page.getByPlaceholder("Items...").fill("Backpack");
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByPlaceholder("Items...").fill("Dresses");
+  await page.getByRole('button', { name: 'Add' }).click();
+await page.waitForTimeout(5000);
+})
---
+++ tests/playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  expect: {
+    timeout: 5000
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
